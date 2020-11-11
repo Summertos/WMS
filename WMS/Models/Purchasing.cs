66264f8fde4b2e193c233c73afd27af9c2588a84
HP <HP@DESKTOP-HNEDNFK>
2020-11-11 10:08:32 +0800

    ll

---
+++ WMS/Models/Purchasing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMS.Models
+{
+    public class Purchasing
+    {
+        public int Purchasing_Id { get; set; }
+        public string Ordernumber { get; set; }
+        public int Supplier { get; set; }
+        public int Category { get; set; }
+        public DateTime purchasedate { get; set; }
+        public string Agent { get; set; }
+        public int State { get; set; }
+    }
+}
